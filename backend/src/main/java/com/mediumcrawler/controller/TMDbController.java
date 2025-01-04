68d794e7bad7e29da26a2964c4d1bd0ef04ea567
kietn20 <kietnguyen3698@gmail.com>
2025-01-03 17:38:37 -0800

    Exposing endpoint to test TMDb integration

---
+++ backend/src/main/java/com/mediumcrawler/controller/TMDbController.java
@@ -0,0 +1,27 @@
+package com.mediumcrawler.controller;
+
+import com.mediumcrawler.dto.MovieDTO;
+import com.mediumcrawler.service.TMDbService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tmdb")
+public class TMDbController {
+
+    private final TMDbService tmdbService;
+
+    public TMDbController(TMDbService tmdbService) {
+        this.tmdbService = tmdbService;
+    }
+
+    @GetMapping("/movies")
+    public Mono<List<MovieDTO>> searchMovies(@RequestParam String query) {
+        return tmdbService.searchMovies(query);
+    }
+}
