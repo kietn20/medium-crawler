76476e4e5876b1ca54c05c7a1df697b15be59745
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-01-10 23:36:40 -0800

    working on instruction modal

--- frontend/src/components/Collage.jsx
+++ frontend/src/components/Collage.jsx
@@ -1,30 +1,61 @@
-import { ChartNoAxesColumnDecreasing } from "lucide-react";
+import { ChartNoAxesColumnDecreasing, Share } from "lucide-react";
 import { Navbar } from "./Navbar";
 import { Slot } from "./Slot";
 
 export const Collage = () => {
   return (
-    <div className="flex flex-col items-center justify-start w-screen h-screen overflow-hidden bg-[#0A0B06]">
+    <div className="relative flex flex-col items-center justify-start w-screen h-screen overflow-hidden bg-[#0A0B06]">
       <Navbar />
-      <span className="font-heading text-9xl text-[#B1FA63]">
-        medium crawler
-      </span>
+      <div className="flex relative w-[1500px] h-32 bg-gray-0 items-center justify-center">
+        <span className="font-heading text-9xl text-[#B1FA63]  absolute bottom-4">
+          medium crawler
+        </span>
+      </div>
+      <div className="absolute top-56 w-[830px] h-[614px] bg-[#B1FA63] flex-col rounded-[30px] border-8 border-[#142120] justify-center z-10">
+        <span className="font-heading text-4xl mt-5 flex justify-center items-center p-7">
+          What is medium crawler?
+        </span>
+        <div className="flex justify-center gap-5">
+          <div className="w-[400px] h-[400px] bg-gray-400 rounded-[30px] flex justify-center items-center font-heading text-white">insert gif here</div>
+          <div className="w-[315px] h-[400px]">
+            <span className="font-heading text-[20px]">
+              Medium Crawler is a creative tool that allows you to curate and
+              display your yearly watchlists in a stunning collage format. Use
+              our intuitive platform to add media from popular sources or input
+              your own.
+              <br />
+              <br />
+              Discover, showcase, and share your favorite media with friends and
+              the community. Whether it's movies, books, TV shows, anime, manga,
+              or games, we’ve got you covered!
+            </span>
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
       <div className="mt-10 w-[822px] h-[614px] grid grid-cols-4 gap-x-[20px] gap-y-[30px]">
         {Array.from({ length: 8 }, (_, i) => (
           <Slot key={i} />
         ))}
       </div>
-      <div className="text-[#B1FA63] flex ">
+      <div className="text-[#B1FA63] flex gap-2 absolute bottom-5 left-5">
+        {/* Instruction Modal */}
         <div className="font-heading flex justify-center items-center text-3xl rounded-full bg-[#142120] w-[50px] h-[50px]">
           <span className="mb-1">?</span>
         </div>
+
         <div className="font-heading flex justify-center items-center text-3xl rounded-full bg-[#142120] w-[50px] h-[50px]">
           <span className="mb-1">
             <ChartNoAxesColumnDecreasing />
           </span>
         </div>
+
         <div className="font-heading flex justify-center items-center text-3xl rounded-full bg-[#142120] w-[50px] h-[50px]">
-          <span className="mb-1">?</span>
+          <span className="mb-1">
+            <Share />
+          </span>
         </div>
       </div>
     </div>
