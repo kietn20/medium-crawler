4358ae2888ebedc9dd4e4cff65f7c08d18de5a27
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-01-11 00:16:42 -0800

    working on help modal

--- frontend/src/components/Collage.jsx
+++ frontend/src/components/Collage.jsx
@@ -13,40 +13,44 @@ export const Collage = () => {
           medium crawler
         </span>
       </div>
-      {showHelp &&
-        <div className="absolute top-56 w-[830px] h-[614px] bg-[#B1FA63] flex-col rounded-[30px] border-8 border-[#142120] justify-center z-10 duration-150">
-          <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
-            What is medium crawler?
-          </span>
-          {/* <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
-          How does medium crawler work?
-          </span> */}
-          <div className="flex justify-center gap-5 relative">
-            <div className="w-[400px] h-[400px] bg-gray-400 rounded-[30px] flex justify-center items-center font-heading text-white">
-              insert gif here
-            </div>
-            <div className="w-[315px] h-[400px]">
-              <span className="font-heading text-[20px]">
-                Medium Crawler is a creative tool that allows you to curate and
-                display your yearly watchlists in a stunning collage format. Use
-                our intuitive platform to add media from popular sources or
-                input your own.
-                <br />
-                <br />
-                Discover, showcase, and share your favorite media with friends
-                and the community. Whether it's movies, books, TV shows, anime,
-                manga, or games, we’ve got you covered!
-              </span>
-            </div>
+      <div
+        className={`absolute top-56 w-[840px] h-[614px] bg-[#B1FA63] flex-col rounded-[30px] border-8 border-[#142120] justify-center z-10 transition-opacity duration-300 ${
+          showHelp
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
+          What is medium crawler?
+        </span>
+        {/* <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
+        How does medium crawler work?
+        </span> */}
+        <div className="flex justify-center gap-5 relative">
+          <div className="w-[400px] h-[400px] bg-gray-400 rounded-[30px] flex justify-center items-center font-heading text-white">
+            insert gif here
           </div>
-          <div className="w-full flex justify-end items-start">
-            <span className="font-heading text-2xl mr-5">
-              &lt; <span>1/2</span> &gt;
+          <div className="w-[315px] h-[400px]">
+            <span className="font-heading text-[20px]">
+              Medium Crawler is a creative tool that allows you to curate and
+              display your yearly watchlists in a stunning collage format. Use
+              our intuitive platform to add media from popular sources or input
+              your own.
+              <br />
+              <br />
+              Discover, showcase, and share your favorite media with friends and
+              the community. Whether it's movies, books, TV shows, anime, manga,
+              or games, we’ve got you covered!
             </span>
           </div>
         </div>
-      }
-      <div className="mt-10 w-[822px] h-[614px] grid grid-cols-4 gap-x-[20px] gap-y-[30px]">
+        <div className="w-full flex justify-end items-start">
+          <span className="font-heading text-2xl mr-5">
+            &lt; <span>1/2</span> &gt;
+          </span>
+        </div>
+      </div>
+      <div className="mt-10 w-[822px] h-[614px] grid grid-cols-4 gap-x-[20px] gap-y-[30px] items-center place-items-center place-content-center">
         {Array.from({ length: 8 }, (_, i) => (
           <Slot key={i} />
         ))}
