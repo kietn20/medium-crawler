65994e4647c42dcbeaeddbb485aa8eeeb635c240
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-01-11 02:27:53 -0800

    finished with help modal

--- frontend/src/components/Collage.jsx
+++ frontend/src/components/Collage.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export const Collage = () => {
   const [showHelp, setShowHelp] = useState(false);
+  const [page, setPage] = useState(1);
   return (
     <div className="relative flex flex-col items-center justify-start w-screen h-screen overflow-hidden bg-[#0A0B06]">
       <Navbar />
@@ -20,33 +21,101 @@ export const Collage = () => {
             : "opacity-0 pointer-events-none"
         }`}
       >
-        <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
-          What is medium crawler?
-        </span>
-        {/* <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
-        How does medium crawler work?
-        </span> */}
-        <div className="flex justify-center gap-5 relative">
-          <div className="w-[400px] h-[400px] bg-gray-400 rounded-[30px] flex justify-center items-center font-heading text-white">
-            insert gif here
+        {page == 1 ? (
+          <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
+            What is medium crawler?
+          </span>
+        ) : (
+          <span className="font-heading text-4xl mt-3 flex justify-center items-center p-7">
+            How does medium crawler work?
+          </span>
+        )}
+        {page == 1 ? (
+          <div className="flex justify-center gap-5 relative">
+            <div className="w-[400px] h-[400px] bg-gray-400 rounded-[30px] flex justify-center items-center font-heading text-white">
+              insert gif here
+            </div>
+            <div className="w-[315px] h-[400px]">
+              <span className="font-heading text-[20px]">
+                Medium Crawler is a creative tool that allows you to curate and
+                display your yearly watchlists in a stunning collage format. Use
+                our intuitive platform to add media from popular sources or
+                input your own.
+                <br />
+                <br />
+                Discover, showcase, and share your favorite media with friends
+                and the community. Whether it's movies, books, TV shows, anime,
+                manga, or games, we’ve got you covered!
+              </span>
+            </div>
           </div>
-          <div className="w-[315px] h-[400px]">
-            <span className="font-heading text-[20px]">
-              Medium Crawler is a creative tool that allows you to curate and
-              display your yearly watchlists in a stunning collage format. Use
-              our intuitive platform to add media from popular sources or input
-              your own.
-              <br />
-              <br />
-              Discover, showcase, and share your favorite media with friends and
-              the community. Whether it's movies, books, TV shows, anime, manga,
-              or games, we’ve got you covered!
-            </span>
+        ) : (
+          <div className="flex justify-center items-center gap-5 relative font-heading">
+            <div>
+              <p className="text-2xl">
+                1. Start a Watchlist
+                <br />
+                <span className="text-xl">
+                  &nbsp;&nbsp;&#8226;&nbsp;&nbsp; Create a watchlist to showcase
+                  your favorite media
+                </span>
+                <br />
+                <span className="text-xl">
+                  &nbsp;&nbsp;&#8226;&nbsp;&nbsp; Add items by searching from
+                  integrated APIs or manually inputting details
+                </span>
+                <br />
+                2. Customize Your Collage
+                <br />
+                <span className="text-xl">
+                  &nbsp;&nbsp;&#8226;&nbsp;&nbsp; Drag, drop, and rearrange
+                  items to your liking
+                </span>
+                <br />
+                <span className="text-xl">
+                  &nbsp;&nbsp;&#8226;&nbsp;&nbsp; Toggle between ranked and
+                  unranked modes for a personalized look
+                </span>
+                <br />
+                3. Save and Share
+                <br />
+                <span className="text-xl">
+                  &nbsp;&nbsp;&#8226;&nbsp;&nbsp; Finalize your watchlist and
+                  share it as a beautiful
+                </span>
+                <br />
+                <span className="text-xl">
+                  &nbsp;&nbsp;&#8226;&nbsp;&nbsp; Post your creation on social
+                  media or send it directly to friends
+                </span>
+                <br />
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="w-full flex justify-end items-start">
+        )}
+        <div className="w-full flex justify-end items-start absolute bottom-2 right-0">
           <span className="font-heading text-2xl mr-5">
-            &lt; <span>1/2</span> &gt;
+            <span
+              className="cursor-pointer text-3xl"
+              onClick={() => {
+                if (page == 2) {
+                  setPage(1);
+                }
+              }}
+            >
+              &lt;&nbsp;
+            </span>
+            <span>{page} / 2</span>
+            <span
+              className="cursor-pointer text-3xl"
+              onClick={() => {
+                if (page == 1) {
+                  setPage(2);
+                }
+              }}
+            >
+              &nbsp;&gt;
+            </span>
           </span>
         </div>
       </div>
@@ -58,8 +127,12 @@ export const Collage = () => {
       <div className="text-[#B1FA63] flex gap-2 absolute bottom-5 left-5">
         {/* Instruction Modal */}
         <div
-          className="font-heading flex justify-center items-center text-3xl rounded-full bg-[#142120] w-[50px] h-[50px] opacity-50 hover:opacity-100 duration-300"
-          onClick={() => setShowHelp(!showHelp)}
+          className="font-heading flex justify-center items-center text-3xl rounded-full bg-[#142120] w-[50px] h-[50px] opacity-50 hover:opacity-100 duration-200"
+          onClick={() => {
+            setPage(1);
+            setShowHelp(!showHelp);
+            console.log("click");
+          }}
           style={{ cursor: "pointer", opacity: showHelp && 1 }}
         >
           <span className="mb-1">?</span>
