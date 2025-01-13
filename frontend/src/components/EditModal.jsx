9a022bf7ca53721cdea28137a1b92b477ba5d018
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-01-12 23:41:02 -0800

    finished edit modal styling

--- frontend/src/components/EditModal.jsx
+++ frontend/src/components/EditModal.jsx
@@ -14,30 +14,61 @@ export const EditModal = () => {
         Edit Media
       </div>
       <div className="flex px-7 justify-between">
-        <div className="w-[250px] h-96 bg-orange-400 bg-opacity-0 flex justify-around items-center flex-col text-md">
+        <div className="w-[250px] h-96 bg-orange-400 bg-opacity-0 flex justify-around items-center flex-col text-[14px]">
           <Slot />
-          <span className="px-4">
-            Drag & Drop an image from the web (e.g. Google Images) or insert web
-            image link into "Web Image URL" section
+          <span className="">
+            Drag & Drop an image from the web (e.g. Google Images) or paste
+            image link into web Web Image URL section
           </span>
         </div>
-        <div className="flex-col justify-between items-center gap-5 w-[350px] h-96 bg-blue-300 bg-opacity-40">
-          <div className="p-1 bg-pink-400 bg-opacity-40 h-80">
-            <form action="/" className="flex-col">
-              <div className="flex w-full bg-red-300 justify-between">
-                <div className="flex-col w-56">
-                  <label htmlFor="">Title*</label>
-                  <input type="text" placeholder="Parasite" className="w-56" />
+        <div className="flex-col justify-start items-start gap-5 w-[350px] h-96 bg-blue-300 bg-opacity-0">
+          <div className="p-1 bg-pink-0 bg-opacity-40 h-80 text-xl">
+            <form action="/" className="flex-col text">
+              <div className="flex w-full justify-between items-center pb-2">
+                <div className="flex-col w-56 justify-between items-center">
+                  <label htmlFor="" className="p-1">
+                    Title*
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Parasite"
+                    className="w-64 text-black"
+                  />
                 </div>
                 <div className="flex-col w-20">
-                  <label htmlFor="">Rating</label>
-                  <input type="text" placeholder="8.5" className="w-14" />
+                  <label htmlFor="" className="p-1">
+                    Rating
+                  </label>
+                  <input type="text" placeholder="8.5" className="w-20" />
                 </div>
               </div>
+
+              <label htmlFor="" className="p-1">
+                Description / Review
+              </label>
+              <textarea
+                name=""
+                id=""
+                placeholder="Enter description here..."
+                className="w-full h-28"
+              ></textarea>
+              <label htmlFor="" className="p-1">
+                Web Image URL
+              </label>
+              <input
+                type="text"
+                className="w-full"
+                placeholder="https://www.imdb.com/title/tt6751668/"
+              />
             </form>
           </div>
-          <div className="flex justify-end items-center h-10 bg-white bg-opacity-40 p-1">
-            save
+          <div className="flex justify-end gap-5 items-center h-10 p-1">
+            <button className="w-32 bg-red-600 bg-opacity-50 hover:bg-opacity-100 duration-150 border rounded-[30px] p-2">
+              Cancel
+            </button>
+            <button className="w-32 bg-[#B1FA63] bg-opacity-50 hover:bg-opacity-100 duration-150 border rounded-[30px] p-2">
+              Save
+            </button>
           </div>
         </div>
       </div>
