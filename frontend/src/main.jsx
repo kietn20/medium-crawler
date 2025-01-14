0201563f0c1238a5fdfc84b7275430a9e3316718
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-01-14 14:57:20 -0800

    wrapped app component in drag and drop context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,12 +6,15 @@ import App from "./App.jsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./services/queryClient";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import DndContext from "./context/DndContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
-	</StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <DndContext>
+        <App />
+      </DndContext>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
 );
