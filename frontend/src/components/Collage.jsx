78938bb9685056a8f2f9067357b8ab637422a050
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-01-14 23:37:34 -0800

    added dock with components

--- frontend/src/components/Collage.jsx
+++ frontend/src/components/Collage.jsx
@@ -1,4 +1,11 @@
-import { ChartNoAxesColumnDecreasing, PencilLine, Share } from "lucide-react";
+import {
+  ChartNoAxesColumnDecreasing,
+  CircleHelp,
+  ListFilterPlus,
+  ListOrdered,
+  PencilLine,
+  Share,
+} from "lucide-react";
 import { Navbar } from "./Navbar";
 import { Slot } from "./Slot";
 import { useEffect, useRef, useState } from "react";
@@ -8,6 +15,7 @@ import { HelpModal } from "./HelpModal";
 import { SearchModal } from "./SearchModal";
 import { EditModal } from "./EditModal";
 import { Toaster } from "react-hot-toast";
+import { Dock, DockIcon, DockItem, DockLabel } from "./UI/Dock";
 
 export const Collage = () => {
   // Help Modal State Store
@@ -27,15 +35,50 @@ export const Collage = () => {
     setHelpButtonRef(helpButtonRef);
   }, [setHelpButtonRef]);
 
+  const dockElements = [
+    {
+      title: "Instructions",
+      icon: (
+        <CircleHelp
+          onClick={() => {
+            setShowHelp(!showHelp);
+            setPage(1);
+          }}
+          className="h-full w-full text-neutral-600 dark:text-neutral-3001 text-[#8ac847]"
+        />
+      ),
+      href: "#",
+    },
+    {
+      title: "Toggle Sorting",
+      icon: (
+        <ListOrdered className="h-full w-full text-neutral-600 dark:text-neutral-3001 text-[#8ac847]" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Share Collage",
+      icon: (
+        <Share className="h-full w-full text-neutral-600 dark:text-neutral-3001 text-[#8ac847]" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Manage Media Lists",
+      icon: (
+        <ListFilterPlus className="h-full w-full text-neutral-600 dark:text-neutral-3001 text-[#8ac847]" />
+      ),
+      href: "#",
+    },
+  ];
+
   return (
     <div className="relative flex flex-col items-center justify-start w-screen h-screen overflow-hidden bg-[#0A0B06]">
       <Navbar />
       <div className="absolute font-heading z-20 text-lg">
         <Toaster />
       </div>
-      <div
-        className="flex relative group h-32 items-center justify-center"
-      >
+      <div className="flex relative group h-32 items-center justify-center">
         <input
           type="text"
           className="font-heading text-8xl text-[#B1FA63] bg-inherit text-center placeholder:text-[#B1FA63] w-[1200px] h-[120px] border-none pb-8"
@@ -50,6 +93,19 @@ export const Collage = () => {
         {mediaItems.map((mediaItem, index) => (
           <Slot key={index} index={index} />
         ))}
+      </div>
+      <div className="absolute bottom-2 left-1/2 max-w-full -translate-x-1/2 opacity-30 hover:opacity-100 duration-300">
+        <Dock className="items-end pb-3">
+          {dockElements.map((item, idx) => (
+            <DockItem
+              key={idx}
+              className="aspect-square rounded-full bg-gray-200 dark:bg-neutral-800 "
+            >
+              <DockLabel>{item.title}</DockLabel>
+              <DockIcon>{item.icon}</DockIcon>
+            </DockItem>
+          ))}
+        </Dock>
       </div>
       <div className="text-[#B1FA63] flex gap-2 absolute bottom-5 left-5">
         <div
