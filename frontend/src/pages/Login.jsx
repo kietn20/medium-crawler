e50718f76d57704836a0f95eb2dee988b98862e8
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-01-15 02:46:22 -0800

    started working on login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,41 @@
-
-
 export const Login = () => {
-
   return (
-    <div className="h-screen overflow-y-scroll bg-[#0A0B06] flex justify-center items-center text-9xl bg-topography-texture">
-      <h1 className="font-heading text-[#B1FA63]">
+    <div className="w-screen h-screen overflow-y-scroll bg-[#0A0B06] flex flex-col justify-center items-center bg-topography-texture gap-10 font-heading">
+      <h1 className="font-heading text-[#B1FA63] text-white text-9xl">
         medium crawler
       </h1>
+      <div className="w-[600px] h-[700px] bg-[#142120] flex flex-col justify-start items-center rounded-[30px] text-white">
+        <span className="text-8xl">Login</span>
+        <form action="" className="flex flex-col justify-center items-center gap-2">
+          <div>
+            <label htmlFor="email" className="text-3xl flex">
+              email
+            </label>
+            <input
+              type="text"
+              className="w-[445px] h-[55px] text-2xl border-none text-[#0A0B06]"
+            />
+          </div>
+          <div className="">
+            <label htmlFor="password" className="text-3xl flex">
+              password
+            </label>
+            <input
+              type="password"
+              className="w-[445px] h-[55px] text-2xl border-none text-[#0A0B06]"
+            />
+          </div>
+          <button className="w-[445px] h-[55px] text-2xl rounded-[20px] bg-white text-[#80978F]">
+            Log in with Google
+          </button>
+          <button className="w-[445px] h-[55px] text-2xl rounded-[20px] bg-white text-[#80978F]">
+            Log in with Github
+          </button>
+          <button className="w-[500px] h-[80px] bg-[#8ac847] text-4xl">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
-  ); 
+  );
 };
