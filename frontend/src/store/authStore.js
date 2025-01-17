37840faf0abee05b6219e179d076d0a5c82af29f
kietn20 <kietnguyen3698@gmail.com>
2025-01-16 19:48:41 -0800

    setting up users accounts

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
-    isSignedIn: false,
-    setSignedIn: (signedIn) => set({ isSignedIn: signedIn }),
+  isSignedIn: false,
+  setSignedIn: (signedIn) => {
+    set({ isSignedIn: signedIn });
+  },
 }));
